3bf20b6a49acc2625f4bc9b81c6e7125bb1b49d3
jskillern <skillernjonathon95@gmail.com>
2025-08-24 11:14:44 -0400

    Message box added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import scrolledtext
 from functions import *  
 
 
@@ -9,19 +10,26 @@ def main():
 
 #window parameters
     root = tk.Tk()
-    root.minsize(250, 100)
-    root.maxsize(500, 800)
+    root.minsize(500, 500)
+    root.maxsize(500, 500)
     root.geometry("500x500")
-    tk.Label(root, text = "To-Do List").grid()
+    tk.Label(root, text = "To-Do List").grid(row=0, column=4, columnspan=4)
 
-#creating buttons
+#creating and placing buttons
     add = tk.Button(root, text="Add", command = add_task)
-    add.grid(row=1, column=0)
+    add.grid(row=1, column=2, columnspan=4)
     
     complete = tk.Button(root, text="Complete", command = complete_task)
-    complete.grid(row=1, column=1)
+    complete.grid(row=1, column=6, columnspan=4)
+
+#creating the message box where the to-do list will be displayed
+    message_box = scrolledtext.ScrolledText(root,wrap=tk.WORD, width= 60, height= 20)
+    message_box.grid(row=2, column=4, columnspan=4)
 
 
+    message_box.insert(tk.END, "Testing \n")
+    #for i in range(len(list)):
+        #message_box.insert(tk.END, f"{task_position}. {i}")
 
 
 
